fa02d9eb7fc8c48c1b5ab5325d7dd2759e781ff6
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-02-28 15:28:50 -0400

    Create client and add to admin

--- apps/people/admin.py
+++ apps/people/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.people.models import Client
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('identification', 'last_name', 'first_name', 'email', 'phone_number')
+
