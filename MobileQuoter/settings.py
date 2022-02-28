1e96fdbbc53a298d3338ab673004d8db03e8d338
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-02-28 10:36:08 -0400

    Add DB settings and create Constants and .env.example files for env variables

--- MobileQuoter/settings.py
+++ MobileQuoter/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from constants import Constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,8 +82,12 @@ WSGI_APPLICATION = 'MobileQuoter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': Constants.DB_NAME,
+        'USER': Constants.DB_USER,
+        'PASSWORD': Constants.DB_PASSWORD,
+        'HOST': Constants.DB_HOST,
+        'PORT': Constants.DB_PORT,
     }
 }
 
