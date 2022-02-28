1a56bda27fa0b60b5c228ba297f5f8fcb7d169af
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-02-28 15:00:19 -0400

    Create Person model

--- apps/people/models.py
+++ apps/people/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from mixins import UpdateMixin
 
-# Create your models here.
+
+class Person(models.Model, UpdateMixin):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
