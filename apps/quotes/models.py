4d27ad2e8cabc0d862688efeee4fce097ae1b1a1
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-02-28 16:57:17 -0400

    Define models for Quote and Invoice

--- apps/quotes/models.py
+++ apps/quotes/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from decimal import Decimal
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+from apps.people.models import Client
+from apps.products.models import Product
+
+
+class Quote(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.PROTECT)
+    products = models.ManyToManyField(Product)
+    generated_date = models.DateField(default=timezone.now)
+    due_date = models.DateField()
+    closed_date = models.DateField(null=True, blank=True)
+    taken = models.BooleanField(default=False)
+    cost = models.DecimalField(max_digits=7, decimal_places=2)
+    extra_info = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'Quote for {self.client}, on {self.generated_date}. {self.extra_info}'
+
+
+class Invoice(models.Model):
+    quote = models.ForeignKey(Quote, on_delete=models.PROTECT)
+    payment = models.DecimalField(max_digits=7, decimal_places=2)
+    balance = models.DecimalField(max_digits=7, decimal_places=2)
