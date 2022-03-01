e627aace0e5fc2f954025f6cc95e4ba9c6092413
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-01 00:25:52 -0400

    Fix pep8 errors in product views

--- apps/products/views.py
+++ apps/products/views.py
@@ -10,43 +10,41 @@ class ProductViewSet(BaseViewSet):
     model = Product
     serializer = BasicProductSerializer
 
-    @swagger_auto_schema(operation_description='Lists Products',
-                         responses={status.HTTP_200_OK: BasicProductSerializer(
-                             many=True)}
-                         )
+    @swagger_auto_schema(
+        operation_description='Lists Products',
+        responses={status.HTTP_200_OK: BasicProductSerializer(many=True)})
     def list(self, request):
         return super().list(request)
 
-    @swagger_auto_schema(operation_description=
-                         'Get details of a Product object',
-                         responses={status.HTTP_200_OK: BasicProductSerializer}
-                         )
+    @swagger_auto_schema(
+        operation_description='Get details of a Product object',
+        responses={status.HTTP_200_OK: BasicProductSerializer})
     def retrieve(self, request, pk=None):
         return super().retrieve(request, pk)
 
-    @swagger_auto_schema(operation_description='Creates Product',
-                         request_body=BasicProductSerializer,
-                         responses={status.HTTP_200_OK: BasicProductSerializer}
-                         )
+    @swagger_auto_schema(
+        operation_description='Creates Product',
+        request_body=BasicProductSerializer,
+        responses={status.HTTP_200_OK: BasicProductSerializer})
     def create(self, request):
         return super().create(request)
 
-    @swagger_auto_schema(operation_description='Updates Product',
-                         request_body=BasicProductSerializer,
-                         responses={status.HTTP_200_OK: BasicProductSerializer}
-                         )
+    @swagger_auto_schema(
+        operation_description='Updates Product',
+        request_body=BasicProductSerializer,
+        responses={status.HTTP_200_OK: BasicProductSerializer})
     def update(self, request):
         return super().update(request)
 
-    @swagger_auto_schema(operation_description='Partially Updates Product',
-                         request_body=BasicProductSerializer,
-                         responses={status.HTTP_200_OK: BasicProductSerializer}
-                         )
+    @swagger_auto_schema(
+        operation_description='Partially Updates Product',
+        request_body=BasicProductSerializer,
+        responses={status.HTTP_200_OK: BasicProductSerializer})
     def partial_update(self, request):
         return super().partial_update(request)
 
-    @swagger_auto_schema(operation_description='Deletes a Product object',
-                         responses={status.HTTP_200_OK: BasicProductSerializer}
-                         )
+    @swagger_auto_schema(
+        operation_description='Deletes a Product object',
+        responses={status.HTTP_200_OK: BasicProductSerializer})
     def destroy(self, request, pk=None):
         return super().destroy(request, pk)
