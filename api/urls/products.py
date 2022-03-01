a184c5c8ab9e02112d2843bfba5e0153ee351e8b
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-01 00:42:23 -0400

    Create product urls

---
+++ api/urls/products.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from apps.products.views import ProductViewSet
+
+people_urls = [
+     path('products',
+          ProductViewSet.as_view({'get': 'list', 'post': 'create'})),
+     path('products/<int:pk>',
+          ProductViewSet.as_view({'get': 'retrieve', 'delete': 'destroy',
+                                  'put': 'update',
+                                  'patch': 'partial_update'})),
+]
