a3c99947f10f0dc6e57458e382dfad6dcfa9f7bd
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-02-28 22:00:43 -0400

    Add SEADED_ELEMENTS env variable

--- constants.py
+++ constants.py
@@ -13,3 +13,6 @@ class Constants:
     SECRET_KEY = config('SECRET_KEY')
     DEBUG = config('DEBUG', cast=bool)
     ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+    # Extra settings
+    SEEDED_ELEMENTS = config('SEEDED_ELEMENTS', cast=int)
