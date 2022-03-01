cf4e45ba4eb8dffd4ef791f951a36312a1024796
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-01 00:24:06 -0400

    Fix pep8 errors in people views

--- apps/people/views.py
+++ apps/people/views.py
@@ -10,37 +10,41 @@ class ClientViewSet(BaseViewSet):
     model = Client
     serializer = BasicClientSerializer
 
-    @swagger_auto_schema(operation_description='Lists Clients',
-                         responses={status.HTTP_200_OK: BasicClientSerializer(
-                             many=True)})
+    @swagger_auto_schema(
+        operation_description='Lists Clients',
+        responses={status.HTTP_200_OK: BasicClientSerializer(many=True)})
     def list(self, request):
         return super().list(request)
 
-    @swagger_auto_schema(operation_description=
-                         'Get details of a Client object',
-                         responses={status.HTTP_200_OK: BasicClientSerializer})
+    @swagger_auto_schema(
+        operation_description='Get details of a Client object',
+        responses={status.HTTP_200_OK: BasicClientSerializer})
     def retrieve(self, request, pk=None):
         return super().retrieve(request, pk)
 
-    @swagger_auto_schema(operation_description='Creates Client',
-                         request_body=BasicClientSerializer,
-                         responses={status.HTTP_200_OK: BasicClientSerializer})
+    @swagger_auto_schema(
+        operation_description='Creates Client',
+        request_body=BasicClientSerializer,
+        responses={status.HTTP_200_OK: BasicClientSerializer})
     def create(self, request):
         return super().create(request)
 
-    @swagger_auto_schema(operation_description='Updates Client',
-                         request_body=BasicClientSerializer,
-                         responses={status.HTTP_200_OK: BasicClientSerializer})
+    @swagger_auto_schema(
+        operation_description='Updates Client',
+        request_body=BasicClientSerializer,
+        responses={status.HTTP_200_OK: BasicClientSerializer})
     def update(self, request):
         return super().update(request)
 
-    @swagger_auto_schema(operation_description='Partially Updates Client',
-                         request_body=BasicClientSerializer,
-                         responses={status.HTTP_200_OK: BasicClientSerializer})
+    @swagger_auto_schema(
+        operation_description='Partially Updates Client',
+        request_body=BasicClientSerializer,
+        responses={status.HTTP_200_OK: BasicClientSerializer})
     def partial_update(self, request):
         return super().partial_update(request)
 
-    @swagger_auto_schema(operation_description='Deletes a Client object',
-                         responses={status.HTTP_200_OK: BasicClientSerializer})
+    @swagger_auto_schema(
+        operation_description='Deletes a Client object',
+        responses={status.HTTP_200_OK: BasicClientSerializer})
     def destroy(self, request, pk=None):
         return super().destroy(request, pk)
